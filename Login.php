daa98a80fd7fa05d1079d257611a1b038d397aba
Dwyngirl <cmpcleao@gmail.com>
2018-03-07 08:33:03 +0000

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("localhost", "id4981639_annoyedcat", "VecC2014!", "id4981639_kamisama");
     
     $username = $_POST["username"];
     $password = $_POST["password"];
